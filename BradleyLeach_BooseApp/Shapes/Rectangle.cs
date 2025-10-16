e43621697c6c0f4f35c09e255c1f4e4da12e152c
BradRl <mrbleach@me.com>
2025-10-16 18:24:10 +0100

    - Added shape abstract class and basic shape classes

---
+++ BradleyLeach_BooseApp/Shapes/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BradleyLeach_BooseApp.Shapes
+{
+    class Rectangle : Shape
+    {
+        protected int width, height;
+
+        public Rectangle(Color colour, int x, int y, int width, int height) : base(colour, x, y)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override void draw(Graphics g)
+        {
+            Pen p = new Pen(this.colour);
+            SolidBrush b = new SolidBrush(this.colour);
+            g.FillRectangle(b, x, y, width, height);
+            g.DrawRectangle(p, x, y, width, height);
+            base.draw(g);
+        }
+    }
+}
---
+++ BradleyLeach_BooseApp/Shapes/Shape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BradleyLeach_BooseApp.Shapes
+{
+    abstract class Shape
+    {
+        protected Color colour;
+        protected int x, y;
+        public Shape(Color colour, int x, int y)
+        {
+            this.colour = colour;
+            this.x = x;
+            this.y = y;
+        }
+
+        public virtual void draw(Graphics g)
+        {
+            Font font = new Font("Comic Sans", 12);
+            Brush brush = new SolidBrush(Color.Black);
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.FormatFlags = StringFormatFlags.NoClip;
+            String text = "Shape";
+            g.DrawString(text, font, brush, this.x, this.y, drawFormat);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
